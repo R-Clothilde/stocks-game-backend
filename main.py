0e19f867a1b41eb93981e99f417ef3b594641930
royan clothilde <royan.clothilde@gmail.com>
2024-01-17 19:13:31 +0100

    V2

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 import random
 from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class CurrentPrices(BaseModel):
     name: str
@@ -15,28 +26,28 @@ async def tableau(nb_appel: int, init: list[CurrentPrices]):
     pourcentageB = random.randint(1, 5)
     pourcentageC = random.randint(1, 5)
     pourcentageD = random.randint(1, 5)
-    tableau = {"SA":0, "SB":0, "SC":0, "SD":0}
+    tableau = []
     print(init)
     if nb_appel%3==0:
         for i in range(len(init)):
             if i == 0:
-                tableau["SA"] = round(float(init[i].mp)-(pourcentageA/100*float(init[i].mp)),2)
+                tableau.append({"name":"StockA", "ticker":"SA", "mp":round(float(init[i].mp)-(pourcentageA/100*float(init[i].mp)),2)})
             elif i ==1:
-                tableau["SB"] = round(float(init[i].mp)-(pourcentageB/100*float(init[i].mp)),2)
+                tableau.append({"name":"StockB", "ticker":"SB", "mp":round(float(init[i].mp)-(pourcentageB/100*float(init[i].mp)),2)})
             elif i == 2:
-                tableau["SC"] = round(float(init[i].mp)-(pourcentageC/100*float(init[i].mp)),2)
+                tableau.append({"name":"StockC", "ticker":"SC", "mp":round(float(init[i].mp)-(pourcentageC/100*float(init[i].mp)),2)})
             else:
-                tableau["SD"] = round(float(init[i].mp)-(pourcentageD/100*float(init[i].mp)),2)
+                tableau.append({"name":"StockD", "ticker":"SD", "mp":round(float(init[i].mp)-(pourcentageD/100*float(init[i].mp)),2)})
                
     else:
         for i in range(len(init)):
             if i == 0:
-                tableau["SA"] = round(float(init[i].mp)+(pourcentageA/100*float(init[i].mp)),2)
+                tableau.append({"name":"StockA", "ticker":"SA", "mp":round(float(init[i].mp)+(pourcentageA/100*float(init[i].mp)),2)})
             elif i ==1:
-                tableau["SB"] = round(float(init[i].mp)+(pourcentageB/100*float(init[i].mp)),2)
+                tableau.append({"name":"StockB", "ticker":"SB", "mp":round(float(init[i].mp)+(pourcentageB/100*float(init[i].mp)),2)})
             elif i == 2:
-                tableau["SC"] = round(float(init[i].mp)+(pourcentageC/100*float(init[i].mp)),2)
+                tableau.append({"name":"StockC", "ticker":"SC", "mp":round(float(init[i].mp)+(pourcentageC/100*float(init[i].mp)),2)})
             else:
-                tableau["SD"] = round(float(init[i].mp)+(pourcentageD/100*float(init[i].mp)),2)
+                tableau.append({"name":"StockD", "ticker":"SD", "mp":round(float(init[i].mp)+(pourcentageD/100*float(init[i].mp)),2)})
             
     return tableau
